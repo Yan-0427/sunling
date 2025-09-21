57a468b34eaf4c48dda12e432bf2631128f66ef8
Yan-0427 <yanyanwang0427@gmail.com>
2025-09-21 14:50:06 +0800

    Update app.js

--- v2/app.js
+++ v2/app.js
@@ -174,11 +174,12 @@ function applyAndRender() {
 
     // 渲染结果
     render(view);
-    renderSources(['all', ...new Set(raw.map(x => x.source))]);
     // 彩蛋：输入 magic 试试看
     if (query === 'magic') {
-      alert('✨ 哇！你发现了隐藏功能！');
-    }
+    alert('✨ 哇！你发现了隐藏功能！');
+}
+    renderSources(['all', ...new Set(raw.map(x => x.source))]);
+ 
 }
 
 /**
