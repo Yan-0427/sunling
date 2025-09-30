6e202915f56169683608f6178661fcf3f8fa3c66
Yan-0427 <yanyanwang0427@gmail.com>
2025-09-30 09:12:39 +0800

    Update app.js

--- v3/app.js
+++ v3/app.js
@@ -422,8 +422,7 @@ list.forEach(item => {
   const itemTags = item[tagsField] || item.tags || [];
   itemTags.forEach(tag => {
     tagCounts[tag] = (tagCounts[tag] || 0) + 1;
-  });
-});  
+  });  
   // 统计每个标签的使用次数
   // TODO: 学员任务 - 实现标签统计功能
   // 提示：需要统计每个标签在当前列表中的使用次数
